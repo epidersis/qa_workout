9ca0fc21f4714bfb7f9ebdccd587ccdf8f43d534
Aleksey Kim <epidersis@gmail.com>
2026-02-01 18:50:40 +0900

    отчет есть, аллур по шагам все выводит. Ура, товарищи (10 allure 34:16)

--- allure_report_intro/tests/test_selene.py
+++ allure_report_intro/tests/test_selene.py
@@ -1,14 +1,22 @@
+import allure
 import pytest
-from selene import be
+from selene import be, browser
 
 
 def test_check_on_qa_workout_pull_requests(driver, base_url):
-    driver.open(base_url)
-    driver.element('a[data-tab-item="repositories"]').click()
-    driver.element('#your-repos-filter').type('qa_workout')
-    # тут бы, конечно, заставить его подождать фильтрации результатов
-    # но суть не в selene
-    driver.element('a[href="/epidersis/qa_workout"]').click()
-    driver.element('span[data-content="Pull requests"]').click()
-    driver.element('a[data-ga-click="Pull Requests, Table state, Closed"]').should(be.visible).click()
-    driver.element('div[aria-label="Issues"]').should(be.not_.blank)
+    with allure.step('Находим репозиторий'):
+        browser.open(base_url)
+        browser.element('a[data-tab-item="repositories"]').click()
+        browser.element('#your-repos-filter').type('qa_workout')
+        # тут бы, конечно, заставить его подождать фильтрации результатов
+        # но суть не в selene
+        browser.element('a[href="/epidersis/qa_workout"]').click()
+
+    with allure.step('Открываем пулл реквесты'):
+        browser.element('span[data-content="Pull requests"]').click()
+
+    with allure.step('Смотрим закрытые'):
+        browser.element('a[data-ga-click="Pull Requests, Table state, Closed"]').should(be.visible).click()
+
+    with allure.step('Проверяем, что список таких реквестов не пустой'):
+        browser.element('div[aria-label="Issues"]').should(be.not_.blank)
