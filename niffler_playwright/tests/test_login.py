7b0459b0e6f9dc508c6e71464b57e5c10cd974e2
Aleksey Kim <epidersis@gmail.com>
2026-02-01 13:47:37 +0900

    removed first half

--- niffler_playwright/tests/test_login.py
+++ niffler_playwright/tests/test_login.py
@@ -11,7 +11,7 @@ from pages.login_page import LoginPage
     ]
 )
 def test_login_success_click(page, base_url, username, password, expected_text):
-    # url = "file:///" + os.path.abspath("index.html")
+    # .path.abspath("index.html")
     login_page = LoginPage(page)
     login_page.locate_success()
     login_page.open(base_url)
