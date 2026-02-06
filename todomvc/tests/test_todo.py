815fb7f14759792bd0f0ecc21cc461dcff6635f2
Aleksey Kim <epidersis@gmail.com>
2026-02-06 09:30:10 +0900

    some construction work in progress

--- todomvc/tests/test_todo.py
+++ todomvc/tests/test_todo.py
@@ -1,11 +1,28 @@
+import time
+
 from selene import browser, have, be
+import pytest
 
 
 def test_addition():
     browser.open('/')
     todo_input_field = browser.element('.new-todo').should(be.blank)
-    todo_input_field.type('walk a dog').press_enter()
-    todo_input_field.type('eat a donut').press_enter()
-    todo_input_field.type('talk with guests').press_enter()
+
+    todos = [
+        'walk a dog',
+        'eat a donut',
+        'talk with guests',
+    ]
+
+    for todo in todos:
+        todo_input_field.type(todo).press_enter()
 
     browser.all('.todo-list>li').with_(timeout=browser.config.timeout * 2).should(have.size(3))
+
+
+def test_check():
+    browser.open('/')
+
+    for i in range(1, 4):
+        browser.element(f'.todo-list>li:nth-child({i})').element('.toggle').click()
+        browser.element(f'.todo-list>li:nth-child({i})').should(have.css_class('completed'))
